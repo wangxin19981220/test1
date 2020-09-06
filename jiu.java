f6ea17657b74779d5f142f4fbcc8755077024817
wangxin19981220 <805242309@qq.com>
2020-09-06 19:11:26 +0800

    test1.1

--- jiu.java
+++ jiu.java
@@ -9,7 +9,7 @@ public class jiu {
 
 	private static void jiu1(int dian, int hua, int li) {
 		if(li==1 && dian==0 && hua==0){
-			count++;
+			count++;//输出结果
 		}
 		if(dian>0) { jiu1(dian-1,hua,li*2);}
 		if(hua>0) {jiu1(dian,hua-1,li-1);}
